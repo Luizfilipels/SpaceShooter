6cc4818fb49243c144451680c10a025c602ab2bc
Luiz Filipe <luiz.lopes@get.inatel.br>
2023-03-30 15:04:17 -0300

    Instanciado duas naves e asteroides

--- src/br/inatel/cdg/Main.java
+++ src/br/inatel/cdg/Main.java
@@ -1,5 +1,28 @@
+/*
+*           --- Space Shooter ---
+*   A nave pode invocar o método atirar(), recebendo como parametro um asteróide
+*   Existem dois tipos de asteróides: Pequeno e Grande
+*   A nave possui dois tipos de tiro: Normal e Explosivo
+*   Asteroides grandes podem ser destruidos apenas com tiro Explosivo
+*   Se o asteroide for destruido, ele chama o metodo destruir(), mostrando uma mensagem
+*
+*/
+
+
+
+
 package br.inatel.cdg;
 
+import br.inatel.cdg.inimigo.Asteroide;
+import br.inatel.cdg.jogador.Nave;
+
 public class Main {
+    Nave rednave = new Nave("Nave Red", 100, "Normal");
+    Nave green_nave = new Nave("Nave Green", 100, "Explosivo");
+
+    Asteroide at1 = new Asteroide("Asteroide 01", "Pequeno");
+    Asteroide at2 = new Asteroide("Asteroide 02", "Grande");
+
+
 
 }
--- src/br/inatel/cdg/jogador/Nave.java
+++ src/br/inatel/cdg/jogador/Nave.java
@@ -1,4 +1,49 @@
 package br.inatel.cdg.jogador;
 
+import br.inatel.cdg.inimigo.Asteroide;
+
 public class Nave {
+    private String nome;
+    private double vida;
+    private String tipoTiro;
+
+    public Nave(String nome, double vida, String tipoTiro) {
+        this.nome = nome;
+        this.vida = vida;
+        this.tipoTiro = tipoTiro;
+
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+    public String getTipoTiro() {
+        return tipoTiro;
+    }
+
+    public void setTipoTiro(String tipoTiro) {
+        this.tipoTiro = tipoTiro;
+    }
+
+    void atirar(Asteroide ast) {
+
+
+    }
+
+
+
 }
